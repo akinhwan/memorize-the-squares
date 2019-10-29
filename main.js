16f290b8cf805495469c97430664892af08f3c3e
Andrew Kim <akinhwan@gmail.com>
2019-10-28 23:43:44 -0400

    count down timer

--- main.js
+++ main.js
@@ -55,6 +55,7 @@ function collectGuess(numBlackened, randomIndexes) {
       if (guesses.indexOf(indexOfNodeList(e.target, squared)) === -1) {
         guesses.push(indexOfNodeList(e.target, squared));
       }
+      e.target.classList.add("blackened");
       console.log(guesses);
     }
 
@@ -68,6 +69,9 @@ function collectGuess(numBlackened, randomIndexes) {
         alert("CORRECT");
       } else {
         alert("WRONG");
+        // show correct
+
+        // x out wrong ones
       }
     }
   });
@@ -105,6 +109,88 @@ function arraysEqual(a, b) {
 // next level
 
 // timer
+function CountDownTimer(duration, granularity) {
+  this.duration = duration;
+  this.granularity = granularity || 1000;
+  this.tickFtns = [];
+  this.running = false;
+}
+
+CountDownTimer.prototype.start = function() {
+  if (this.running) {
+    return;
+  }
+  this.running = true;
+  var start = Date.now(),
+    that = this,
+    diff,
+    obj;
+
+  (function timer() {
+    diff = that.duration - (((Date.now() - start) / 1000) | 0);
+
+    if (diff > 0) {
+      setTimeout(timer, that.granularity);
+    } else {
+      diff = 0;
+      that.running = false;
+    }
+
+    obj = CountDownTimer.parse(diff);
+    that.tickFtns.forEach(function(ftn) {
+      ftn.call(this, obj.minutes, obj.seconds);
+    }, that);
+  })();
+};
+
+CountDownTimer.prototype.onTick = function(ftn) {
+  if (typeof ftn === "function") {
+    this.tickFtns.push(ftn);
+  }
+  return this;
+};
+
+CountDownTimer.prototype.expired = function() {
+  return !this.running;
+};
+
+CountDownTimer.prototype.timesExpired = 0;
+
+CountDownTimer.parse = function(seconds) {
+  return {
+    minutes: (seconds / 60) | 0,
+    seconds: seconds % 60 | 0
+  };
+};
+
+// Create new Timer
+
+var display = document.querySelector("#timer"),
+  timer = new CountDownTimer(10);
+
+timer
+  .onTick(format)
+  .onTick(restart)
+  .start();
+
+function restart() {
+  console.log(this.timesExpired);
+  if (this.timesExpired == 2) {
+    this.running = false;
+    alert("TIME OUT");
+  } else if (this.expired()) {
+    flipOver();
+    this.timesExpired++;
+    console.log(this.timesExpired);
+    timer.start();
+  }
+}
+
+function format(minutes, seconds) {
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  display.textContent = minutes + ":" + seconds;
+}
 
 initGrid(5);
 fillIn(5);
